6264f26aac00dc600a8ab8d09f6b339d37015c9d
SandraYushko <shyria6@gmail.com>
2023-04-04 23:54:10 +0300

    it-kamasutra 04/04/2023

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Header from './header';
 
 const App = () => {
   return (
-    <div>
-      <div> <Header /></div>
-      <div className="App">
-        <ul>
-          <li>css</li>
-          <li>html</li>
-          <li>js</li>
-          <li>react</li>
-        </ul>
-      </div>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9fdx0nfz_0nR5-X3J5PC18FD7Q6I3s_C_YQ&usqp=CAU'></img>
+      </header>
+      <nav className='nav'>
+        <div>Profile</div>
+        <div>Messages</div>
+      </nav>
+      <div className='content'>Main content</div>
     </div>
   );
 }
